e06cf63f513f30aca71d51be34adf230734aca03
ElidaR1 <reis.elida@escola.pr.gov.br>
2025-09-16 10:30:09 -0300

    16/09

---
+++ perguntas.js
@@ -0,0 +1,87 @@
+criacartao(
+    'Conhecimentos gerais',
+    'Quantas cores tem o arco-íris?',
+    'Sete'
+)
+
+criacartao(
+    'Conhecimentos gerais',
+    'Primogênito é o nome dado a qual filho de um casal?',
+    'Primeiro filho'
+)
+
+criacartao(
+    'Conhecimentos gerais',
+    'Qual é a letra que antecede a letra O no alfabeto brasileiro?    ',
+    'N'
+)
+
+criacartao(
+    'Conhecimentos gerais',
+    'Quantas patas uma formiga possui?',
+    '6'
+)
+
+criacartao(
+    'Conhecimentos gerais',
+    'Os faraós eram governantes de qual país?',
+    'Egito'
+)
+
+criacartao(
+    'Ciências da terra',
+    'Qual é o nome do único satélite natural da Terra?',
+    'Lua'
+)
+
+criacartao(
+    'Zoologia',
+    ' Qual é o único país onde o animal coala vive?',
+    'Austrália'
+)
+
+criacartao(
+    'Conhecimentos gerais',
+    'As cores vermelho e azul misturadas formam qual cor?',
+    'Roxo'
+)
+
+criacartao(
+    'Conhecimentos gerais',
+    'Quantos anos tem um século?',
+    '100'
+)
+
+criacartao(
+    'Conhecimentos gerais',
+    'Quem pintou o quadro Mona Lisa?',
+    'Leonardo da Vinci'
+)
+
+criacartao(
+    'Adivinhas',
+    'O que é, o que é? Feito para andar e não anda?',
+    'A rua'
+)
+
+
+criacartao(
+    'Adivinhas',
+    'O que é, o que é? Dá muitas voltas e não sai do lugar?',
+    'O relógio'
+)
+
+criacartao(
+    'Adivinhas',
+    'O que é, o que é? Anda com os pés na cabeça?',
+    'O piolho'
+)
+
+criacartao(
+    'Adivinhas',
+    'O que é, o que é? Dá muitas voltas e não sai do lugar?',
+    'O relógio'
+)
+
+
+
